f39c10bdf5838c928c467c27d93a1ba5e0028303
Josius <josius.amaro@gmail.com>
2023-07-07 14:20:14 -0300

    configura hr-worker como um spcloud config server client

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 #Estudos-Microsservicos-Java-Spring-Boot-Spring-Cloud
 
+OBSERVAÇÕES
+- para o bootstrap.properties funcionar, precisa dessa dependência no pom.xml:
+```xml
+<dependency>
+    <groupId>org.springframework.cloud</groupId>
+    <artifactId>spring-cloud-starter-bootstrap</artifactId>
+</dependency>
+```
+
 TODO
 - configurar timeout tanto do resilience4j quanto do spring cloud gateway
 -- Curiosamente, não consegui reproduzir o erro 504 timeout de gateway, acredito que os timeouts já venham configurados para o caso, com espera de 1 minuto (60000 milissegundos)
--- hr-worker/src/main/java/com/microservices/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/microservices/hrworker/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,11 +22,23 @@ public class WorkerResource {
 
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 	
+	// annotation abaixo serve para recuperar uma configuração, no caso, recuperará do repositório do github, do arquivo ht-worker.properties o valor de test.config
+	@Value("${test.config}")
+	private String testConfig;
+
 	@Autowired
 	private Environment env;
 	
 	@Autowired
 	private WorkerRepository repository;
+
+	// endpoint para recuperar as configurações do servidor de configurações hr-config-server
+	@GetMapping(value = "/configs")
+	public ResponseEntity<Void> getConfigs() {
+
+		logger.info("CONFIG= " + testConfig);
+		return ResponseEntity.noContent().build();
+	}
 
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll() {
