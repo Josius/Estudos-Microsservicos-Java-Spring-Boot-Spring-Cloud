77d0f1ef71e0be7d6c22f491a0b79c6311e04d00
Josius <josius.amaro@gmail.com>
2023-07-10 09:41:41 -0300

    configura user feign client

---
+++ hr-oauth/src/main/java/com/microservice/hroauth/entities/User.java
@@ -0,0 +1,23 @@
+package com.microservice.hroauth.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User implements Serializable{
+	
+	private static final long serialVersionUID = 1l;
+	
+	private Long id;
+	private String name;
+	private String email;
+	private String password;
+	private Set<Role> roles = new HashSet<>();
+}
